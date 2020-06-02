c5acb57602543ba7081071283590e5e06eb32532
Mats <mantis3dp@posteo.de>
2020-06-02 21:52:27 +0200

    addedadnw keymap in planck

--- keyboards/matspad/keymaps/adnw/keymap.c
+++ keyboards/matspad/keymaps/adnw/keymap.c
@@ -3,12 +3,21 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-	    DE_LPRN, DE_LBRC, DE_RBRC, DE_RPRN, KC_CALC,    _______,
-	    KC_BSPC, DE_EQL,  DE_SLSH, DE_ASTR, KC_DEL,     KC_F4,
-	    KC_COMM, KC_P7,   KC_P8,   KC_P9,   KC_DOT,     KC_TAB,
-	    DE_MINS, KC_P4,   KC_P5,   KC_P6,   DE_PLUS,    KC_LSFT,
-	    KC_ENT,  KC_P1,   KC_P2,   KC_P3,   KC_UP,      KC_F2,
-	    DE_H,    DE_Q,    KC_P0,   KC_LEFT, KC_DOWN,    KC_RGHT),
+	    DE_LPRN,        DE_LBRC, DE_RBRC, DE_RPRN, _______,    DE_EQL,
+	    DE_MINS,        DE_SLSH, DE_ASTR, KC_BSPC, KC_DEL,     KC_F4,
+	    KC_TAB,        KC_P7,   KC_P8,   KC_P9,   KC_DOT,     KC_LSFT,
+	    DE_PLUS,        KC_P4,   KC_P5,   KC_P6,   KC_COMM,    KC_SPC,
+	    LT(1, KC_ENT),  KC_P1,   KC_P2,   KC_P3,   KC_UP,      KC_F2,
+	    _______,        _______, KC_P0,   KC_LEFT, KC_DOWN,    KC_RGHT),
+
+	[1] = LAYOUT(
+	    RESET,      _______,    _______,    _______,    _______,  _______,
+	    _______,    _______,    _______,    _______,    _______,  _______,
+	    _______,    _______,    _______,    _______,    _______,  _______,
+	    LCTL(KC_X), _______,    _______,    _______,    _______,  _______,
+	    _______,    LCTL(KC_C), LCTL(KC_V), _______,    KC_VOLU,  _______,
+	    _______,    _______,    KC_MPLY,    KC_MPRV,    KC_VOLD,  KC_MNXT),
 };
+
 
 
