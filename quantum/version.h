b55e1c61fc1ebe634a8aea4b89ee41a40a8300f6
Mantis-3DP <mantis3dp@posteo.de>
2021-06-02 23:40:05 +0200

    idontknow

---
+++ quantum/version.h
@@ -0,0 +1,4 @@
+#define QMK_VERSION "0.11.69-28-gb97b6c-dirty"
+#define QMK_BUILDDATE "2021-05-31-15:49:29"
+#define CHIBIOS_VERSION "breaking_2020_q1"
+#define CHIBIOS_CONTRIB_VERSION "breaking_2020_q1"
